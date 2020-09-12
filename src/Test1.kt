f8e1fff4ff33d6fb033d3c991fe2713cdfbac92c
nayan_meher <nayanmeher16@gmail.com>
2020-09-12 22:04:22 +0530

    testing higher order function

--- src/Test1.kt
+++ src/Test1.kt
@@ -1,4 +1,24 @@
+
+fun filter(list: List<String>, checkLength:((String)->Int)){
+    //use of higher order function
+    /*list
+        .forEach {
+           val length: Int = checkLength(it)
+            println("$it -> $length")
+
+    }*/
+
+    //function given by kotlin to modify the collection
+    list.associate {
+        it to it.length
+    }.forEach{
+        println("${it.key} --> ${it.value}")
+    }
+}
+
 fun main() {
-    println("Hello world, This is my first kotlin file.")
+    //println("Hello world, This is my first kotlin file.")
+    val list = listOf("audi", "mercedes", "bmw", "Lamborghini", "aston martin", "Maserati")
+    filter(list) { it.length }
 }
 
